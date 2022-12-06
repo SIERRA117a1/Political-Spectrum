1858d5d07f01c91e80f35b165021190643bbf1bb
xRaGe <69633507+SIERRA117a1@users.noreply.github.com>
2022-12-06 23:53:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from tensorflow import keras
 #classifier = pickle.load(open('D:\Mini Project\Political Spectrum\model.pkl', 'rb'))
 #C:\Users\Administrator\Desktop\Mini Project\Political Spectrum\model.pkl
 #classifier = joblib.load(open(r'D:\Mini Project\Political Spectrum\model.pkl', 'rb'))
-classiifer = keras.models.load_model('D:\Mini Project\Political Spectrum\model')
+classiifer = keras.models.load_model('\model\')
 def welcome():
 	return 'welcome all'
 
